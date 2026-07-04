96cdddb577a19bdf6cf1dbef8650a52359a47cf0
Fovir <fovir@disroot.org>
2026-07-04 23:57:17 +0800

    chore: detect file language
    Signed-off-by: Fovir <fovir@disroot.org>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 mod cli;
 mod detector;
+use anyhow::anyhow;
+use clap::Parser;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let cli = cli::Cli::parse();
-    println!("Hello, world!")
+    let lang = detector::detect(&cli.output)
+        .ok_or_else(|| anyhow!("unsupported file type: {:?}", cli.output))?;
+    println!("language is {}", lang);
+
+    Ok(())
 }
