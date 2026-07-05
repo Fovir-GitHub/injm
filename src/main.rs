c3e0d2da882415576c1d78e297fc12ddf82fd53d
Fovir <fovir@disroot.org>
2026-07-05 17:44:32 +0800

    feat: check whether the output file exists
    Signed-off-by: Fovir <fovir@disroot.org>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 mod cli;
 mod detector;
+mod extractor;
 
 use clap::Parser;
 use std::fs;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = cli::Cli::parse();
+
+    if !fs::exists(&cli.output)? {
+        return Err("file does not exist".into());
+    }
+
     let lang = detector::detect(&cli.output)?;
     println!("language is {}", lang);
 
