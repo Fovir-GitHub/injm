07f2aa2d44556af6fdb2f5e5f5b94ec672fe2249
Fovir <fovir@disroot.org>
2026-07-05 21:14:59 +0800

    feat(io): provide a function to read stdin into a string
    Signed-off-by: Fovir <fovir@disroot.org>

---
+++ src/io.rs
@@ -0,0 +1,8 @@
+use crate::error::Result;
+use std::io::{self, Read};
+
+pub fn read_stdin() -> Result<String> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+    Ok(input)
+}
