cc8d0db11ca0674ce5cb95f560cd971c29e796cd
Fovir <fovir@disroot.org>
2026-07-07 19:37:32 +0800

    chore: bump to `v0.2.0`
    Signed-off-by: Fovir <fovir@disroot.org>

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "injm";
-          version = "0.1.1";
+          version = "0.2.0";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
           preBuild = ''
